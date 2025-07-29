e6b4825c95ce7049d2efe890cac1b5b7006342ee
Betsy-tr <betsytramma@gmail.com>
2025-07-29 11:55:08 -0400

    update

---
+++ app/Models/Article.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Article extends Model
+{
+    // colones autoriser en écriture
+    use HasFactory; 
+    protected $fillable =[
+        'tittle',
+        'content',
+        'publish',
+    ];
+
+    protected $cast = [
+        'publish'=> 'boolean',
+
+    ];
+}
---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Article;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //creation de 50article aleatoire
+        Article::factory(50)->create();
+    }
+}
